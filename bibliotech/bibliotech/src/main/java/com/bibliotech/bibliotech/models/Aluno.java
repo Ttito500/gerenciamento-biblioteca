fda285f437306313f3c24a66469452a2896327f8
Luis Dallas <luiseduard2005@hotmail.com>
2024-12-17 00:46:14 -0300

    EAGER

--- bibliotech/bibliotech/src/main/java/com/bibliotech/bibliotech/models/Aluno.java
+++ bibliotech/bibliotech/src/main/java/com/bibliotech/bibliotech/models/Aluno.java
@@ -17,7 +17,7 @@ public class Aluno {
     @Column(name = "id", nullable = false)
     private Integer id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "id_turma", nullable = false)
     private com.bibliotech.bibliotech.models.Turma idTurma;
