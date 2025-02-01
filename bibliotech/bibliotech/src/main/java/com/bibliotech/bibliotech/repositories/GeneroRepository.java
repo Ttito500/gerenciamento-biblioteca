ce15331b4f45fa71eb11513b6b19725ed0c7afe2
GabrielAlves-Dev <pessoa80gabriel@gmail.com>
2025-02-01 01:00:22 -0300

    nullable in genero

--- bibliotech/bibliotech/src/main/java/com/bibliotech/bibliotech/repositories/GeneroRepository.java
+++ bibliotech/bibliotech/src/main/java/com/bibliotech/bibliotech/repositories/GeneroRepository.java
@@ -1,6 +1,7 @@
 package com.bibliotech.bibliotech.repositories;
 
 import com.bibliotech.bibliotech.models.Genero;
+import jakarta.annotation.Nullable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,7 +11,7 @@ import java.util.List;
 public interface GeneroRepository extends JpaRepository<Genero, Integer> {
     @Query("SELECT g FROM Genero g " +
             "WHERE (:genero IS NULL OR LOWER(g.genero) LIKE LOWER(CONCAT('%', :genero, '%')))")
-    List<Genero> filtrarGeneros(@Param("genero") String genero);
+    List<Genero> filtrarGeneros(@Param("genero") @Nullable String genero);
 
     @Query("SELECT g FROM Genero g WHERE NOT EXISTS (SELECT 1 FROM Livrogenero lg WHERE lg.idGenero = g) AND NOT EXISTS (SELECT 1 FROM Secaogenero sg WHERE sg.idGenero = g)")
     List<Genero> findGenerosSemAssociacao();
