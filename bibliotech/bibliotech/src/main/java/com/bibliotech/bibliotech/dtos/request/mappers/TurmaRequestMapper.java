f8989a356933c0acb9ed4219a1e49d21bf86b1eb
Tiago Tito <tiago.tito.sampaio@gmail.com>
2025-02-03 08:22:11 -0300

    turmaDTOrequestWIP

---
+++ bibliotech/bibliotech/src/main/java/com/bibliotech/bibliotech/dtos/request/mappers/TurmaRequestMapper.java
@@ -0,0 +1,26 @@
+package com.bibliotech.bibliotech.dtos.request.mappers;
+
+import com.bibliotech.bibliotech.repositories.TurmaRepository;
+import com.bibliotech.bibliotech.models.Turma;
+import com.bibliotech.bibliotech.dtos.request.TurmaRequestDTO;
+
+public class TurmaRequestMapper {
+    private final TurmaRepository turmaRepository;
+
+    public TurmaRequestMapper(TurmaRepository turmaRepository) {this.turmaRepository = turmaRepository;}
+
+    public Turma toEntity(TurmaRequestDTO turmaRequestDTO) {
+        if (turmaRequestDTO == null) {
+            return null;
+        }
+
+        Turma turma = new Turma();
+        turma.setId(turmaRequestDTO.getId());
+        turma.setSerie(turmaRequestDTO.getSerie());
+        turma.setTurma(turmaRequestDTO.getTurma());
+        turma.setAnoDeEntrada(turmaRequestDTO.getAnoDeEntrada());
+
+        return turma;
+    }
+
+}
