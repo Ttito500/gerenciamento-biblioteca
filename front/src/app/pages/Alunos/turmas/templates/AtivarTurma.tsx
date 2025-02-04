7fc254943c78b70e3ec3176e63842f705639f2d0
Pedro Rivaldo <pedrorivaldev@gmail.com>
2025-02-04 14:16:18 -0300

    formatando tabs de turma

--- front/src/app/pages/Alunos/turmas/templates/AtivarTurma.tsx
+++ front/src/app/pages/Alunos/turmas/templates/AtivarTurma.tsx
@@ -3,16 +3,16 @@ import Col from "react-bootstrap/esm/Col";
 import Form from "react-bootstrap/esm/Form";
 
 const AtivarTurma: React.FC = () => {
-  return(
-      <Form>
-          <Row>
-              <Col xs={10}>
-                  <Form.Group className="mb-3">
-                      <Form.Label>Tem certeza que deseja ativar essa Turma?</Form.Label>
-                  </Form.Group>
-              </Col>
-          </Row>
-      </Form>
+  return (
+    <Form>
+      <Row>
+        <Col xs={10}>
+          <Form.Group className="mb-3">
+            <Form.Label>Tem certeza que deseja ativar essa Turma?</Form.Label>
+          </Form.Group>
+        </Col>
+      </Row>
+    </Form>
   );
 };
 
--- front/src/app/pages/Alunos/turmas/templates/EditarTurma.tsx
+++ front/src/app/pages/Alunos/turmas/templates/EditarTurma.tsx
@@ -5,48 +5,44 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/esm/Button";
 
 const EditarTurma: React.FC = () => {
+  return (
+    <Form>
+      <Row>
+        <Col xs={3}>
+          <Form.Group className="mb-3">
+            <Form.Label>
+              Série <span className="obgr">*</span>
+            </Form.Label>
+            <Form.Control type="number" placeholder="Digite a série" required />
+          </Form.Group>
+        </Col>
 
+        <Col xs={3}>
+          <Form.Group className="mb-3">
+            <Form.Label>
+              Turma <span className="obgr">*</span>
+            </Form.Label>
+            <Form.Control type="text" placeholder="Digite a turma" required />
+          </Form.Group>
+        </Col>
 
-    return(
-        <Form>
-            <Row>
-                <Col xs={3}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>
-                            Série <span className="obgr">*</span>
-                        </Form.Label>
-                        <Form.Control type="number" placeholder="Digite a série" required />
-                    </Form.Group>
-                </Col>
+        <Col xs={3}>
+          <Form.Group className="mb-3">
+            <Form.Label>
+              Ano de Entrada <span className="obgr">*</span>
+            </Form.Label>
+            <Form.Control type="number" placeholder="Digite o ano" required />
+          </Form.Group>
+        </Col>
+      </Row>
 
-                <Col xs={3}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>
-                            Turma <span className="obgr">*</span>
-                        </Form.Label>
-                        <Form.Control type="text" placeholder="Digite a turma" required />
-                    </Form.Group>
-                </Col>
-
-                <Col xs={3}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>
-                            Ano de Entrada <span className="obgr">*</span>
-                        </Form.Label>
-                        <Form.Control type="number" placeholder="Digite o ano" required />
-                    </Form.Group>
-                </Col>
-            </Row>
-
-            <Row>
-                <Col className="d-flex justify-content-end">
-                    <Button variant="success">Ativar Turma</Button>
-                </Col>
-
-            </Row>
-        </Form>
-
-    );
+      <Row>
+        <Col className="d-flex justify-content-end">
+          <Button variant="success">Ativar Turma</Button>
+        </Col>
+      </Row>
+    </Form>
+  );
 };
 
 export default EditarTurma;
