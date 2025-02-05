9b285f385ecb1ade99980285068c22487899a8dd
kauan <kauancavalcante8p@gmail.com>
2025-02-05 02:06:42 -0300

    modal de exemplar, cadastro e listagem

---
+++ front/src/app/pages/Acervo/Exemplares/Exemplares.tsx
@@ -0,0 +1,179 @@
+import React, { useEffect, useState } from "react";
+import ToastContainer from "react-bootstrap/esm/ToastContainer";
+import Toast from "react-bootstrap/esm/Toast";
+import Button from "react-bootstrap/esm/Button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheck} from "@fortawesome/free-solid-svg-icons";
+import Accordion from "react-bootstrap/esm/Accordion";
+import Form from "react-bootstrap/esm/Form";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import ListagemExemplares from "./templates/ListagemExemplares";
+
+const Exemplares: React.FC = () => {
+
+    const [loading, setLoading] = useState<boolean>(true);
+    const [showToastError, setShowToastError] = useState(false);
+    const [showToastSuccess, setShowToastSuccess] = useState(false);
+
+    useEffect(() => {
+        telaTurmas();
+    }, []);
+
+    const telaTurmas = async (): Promise<void> => {
+        setLoading(true);
+
+        try {
+            /* empty */
+        } catch (err) {
+            setShowToastError(true);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <section className="Exemplar">
+            <div className="Exemplar-acoes">
+                <ToastContainer
+                    className="p-3"
+                    position="bottom-center"
+                    style={{zIndex: 10}}
+                >
+                    <Toast
+                        bg="success"
+                        onClose={() => setShowToastSuccess(false)}
+                        show={showToastSuccess}
+                        delay={3000}
+                        autohide
+                    >
+                        <Toast.Header>
+                            <strong className="me-auto">
+                                Operação realizada com sucesso!
+                            </strong>
+                        </Toast.Header>
+                    </Toast>
+                </ToastContainer>
+
+                <ToastContainer
+                    className="p-3"
+                    position="bottom-center"
+                    style={{zIndex: 10}}
+                >
+                    <Toast
+                        bg="danger"
+                        onClose={() => setShowToastError(false)}
+                        show={showToastError}
+                        delay={3000}
+                        autohide
+                    >
+                        <Toast.Header>
+                            <strong className="me-auto">
+                                Não foi possível concluir a operação. Tente novamente.
+                            </strong>
+                        </Toast.Header>
+                    </Toast>
+                </ToastContainer>
+
+                <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Body className="accordion-body-expanded">
+                            <strong>Cadastro de Exemplares do Livro:</strong>
+                            <Form className="mt-3">
+                                <Row>
+                                    <Col>
+                                        <Form.Group className="mb-6">
+                                            <Form.Label>
+                                                Estante <span className="obgr">*</span>
+                                            </Form.Label>
+                                            <Form.Select aria-label="Selecione">
+                                                <option>Selecione</option>
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                            </Form.Select>
+                                            <Form.Control.Feedback type="invalid">
+                                                Campo obrigatório.
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                    </Col>
+                                    <Col>
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>
+                                                Prateleira <span className="obgr">*</span>
+                                            </Form.Label>
+                                            <Form.Select aria-label="Selecione">
+                                                <option>Selecione</option>
+                                                <option value="1">A</option>
+                                                <option value="2">B</option>
+                                                <option value="3">C</option>
+                                            </Form.Select>
+                                            <Form.Control.Feedback type="invalid">
+                                                Campo obrigatório.
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                    </Col>
+                                    <Col>
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>
+                                                Seção <span className="obgr">*</span>
+                                            </Form.Label>
+                                            <Form.Select aria-label="Selecione">
+                                                <option>Selecione</option>
+                                                <option value="1">X</option>
+                                                <option value="2">Y</option>
+                                                <option value="3">Z</option>
+                                            </Form.Select>
+                                            <Form.Control.Feedback type="invalid">
+                                                Campo obrigatório.
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                    </Col>
+                                    <Col>
+                                        <Form.Group className="mb-10">
+                                            <Form.Label>
+                                                Quantidade de Exemplares <span className="obgr">*</span>
+                                            </Form.Label>
+                                            <Form.Control
+                                                type="number"
+                                                placeholder="Digite a quantidade"
+                                            />
+                                            <Form.Control.Feedback type="invalid">
+                                                Campo obrigatório.
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Observação</Form.Label>
+                                            <Form.Control
+                                                as="textarea"
+                                                rows={1}
+                                                placeholder="Digite uma observação"
+                                            />
+                                        </Form.Group>
+                                    </Col>
+                                    <Col>
+                                        <div className="w-100 h-100 d-flex justify-content-end align-items-end">
+                                            <Button variant="success">
+                                                <FontAwesomeIcon icon={faCheck} /> Cadastrar Novo(s) Exemplar(es)
+                                            </Button>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Form>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            </div>
+
+            <div className="w-100">
+                <ListagemExemplares/>
+            </div>
+        </section>
+    );
+};
+
+export default Exemplares;
---
+++ front/src/app/pages/Acervo/Exemplares/templates/ListagemExemplares.tsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import { Table, Badge, Button, ButtonGroup } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faEdit } from "@fortawesome/free-solid-svg-icons";
+import Modal from "react-bootstrap/esm/Modal";
+import EditarExemplar from "./EditarExemplar";
+
+const ListagemExemplares: React.FC = () => {
+
+    const [showEditar, setShowEditar] = useState(false);
+    const handleCloseEditar = () => setShowEditar(false);
+    const handleShowEditar = () => setShowEditar(true);
+
+
+    return(
+      <>
+          <p className="fw-bold">Exemplares do Livro:</p>
+
+          <Table striped className="tabela">
+              <thead>
+              <tr>
+                  <th>Exemplar</th>
+                  <th>Estante</th>
+                  <th>Prateleira</th>
+                  <th>Seção</th>
+                  <th>Observações</th>
+                  <th>Situação</th>
+                  <th>Ações</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr className="tabela-tr">
+                  <td>1</td>
+                  <td>5</td>
+                  <td>3</td>
+                  <td>Literatura</td>
+                  <td>Bom estado</td>
+                  <td>
+                      <Badge className="bibliotech-badge" bg="green">
+                          Disponível
+                      </Badge>
+                  </td>
+                  <td>
+                      <ButtonGroup aria-label="Ações" className="tabela-acoes">
+                          <Button
+                              onClick={handleShowEditar}
+                              variant="btn-outline-secondary"
+                              className="color-green"
+                          >
+                              <FontAwesomeIcon icon={faEdit} />
+                          </Button>
+                      </ButtonGroup>
+                  </td>
+              </tr>
+              <tr className="tabela-tr">
+                  <td>2</td>
+                  <td>2</td>
+                  <td>1</td>
+                  <td>Ciências</td>
+                  <td>Pequenos desgastes</td>
+                  <td>
+                      <Badge className="bibliotech-badge" bg="blue">
+                          Emprestado
+                      </Badge>
+                  </td>
+                  <td>
+                      <ButtonGroup aria-label="Ações" className="tabela-acoes">
+                          <Button
+                              onClick={handleShowEditar}
+                              variant="btn-outline-secondary"
+                              className="color-green"
+                          >
+                              <FontAwesomeIcon icon={faEdit} />
+                          </Button>
+                      </ButtonGroup>
+                  </td>
+              </tr>
+              <tr className="tabela-tr">
+                  <td>3</td>
+                  <td>1</td>
+                  <td>4</td>
+                  <td>História</td>
+                  <td>Danificado</td>
+                  <td>
+                      <Badge className="bibliotech-badge" bg="red">
+                          Extraviado
+                      </Badge>
+                  </td>
+                  <td>
+                      <ButtonGroup aria-label="Ações" className="tabela-acoes">
+                          <Button
+                              onClick={handleShowEditar}
+                              variant="btn-outline-secondary"
+                              className="color-green"
+                          >
+                              <FontAwesomeIcon icon={faEdit} />
+                          </Button>
+                      </ButtonGroup>
+                  </td>
+              </tr>
+              </tbody>
+          </Table>
+
+          <Modal
+              show={showEditar}
+              onHide={handleCloseEditar}
+              size="lg"
+              backdrop="static"
+              centered
+              keyboard={false}
+          >
+              <Modal.Header closeButton>
+                  <Modal.Title>Editar Exemplar</Modal.Title>
+              </Modal.Header>
+
+              <Modal.Body>
+                  <EditarExemplar />
+              </Modal.Body>
+
+              <Modal.Footer>
+                  <Button variant="secondary" onClick={handleCloseEditar}>
+                      Cancelar
+                  </Button>
+                  <Button variant="success">
+                      <FontAwesomeIcon icon={faCheck} /> Salvar
+                  </Button>
+              </Modal.Footer>
+          </Modal>
+      </>
+  );
+};
+
+export default ListagemExemplares;
