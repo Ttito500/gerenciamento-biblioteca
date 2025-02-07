3f6abf8c50f06f14f443b8184c9fe80723d7a03f
Tiago Tito <tiago.tito.sampaio@gmail.com>
2025-02-06 21:02:14 -0300

    response DTO Usuario

---
+++ bibliotech/bibliotech/src/main/java/com/bibliotech/bibliotech/dtos/response/mappers/UsuarioResponseMapper.java
@@ -0,0 +1,23 @@
+package com.bibliotech.bibliotech.dtos.response.mappers;
+
+import com.bibliotech.bibliotech.dtos.response.UsuarioResponseDTO;
+import com.bibliotech.bibliotech.models.Usuario;
+
+public class UsuarioResponseMapper {
+    public UsuarioResponseDTO toDto(Usuario usuario) {
+        if (usuario == null) {
+            return null;
+        }
+
+        UsuarioResponseDTO dto = new UsuarioResponseDTO();
+        dto.setId(usuario.getId());
+        dto.setNome(usuario.getNome());
+        dto.setCargo(usuario.getCargo());
+        dto.setAtivo(usuario.getAtivo());
+        dto.setEmail(usuario.getEmail());
+        dto.setDataUltimoAcesso(usuario.getDataUltimoAcesso());
+
+        return dto;
+    }
+
+}
