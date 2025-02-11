cb4dc2a0ec68ff21ef18a10be035572052e0068d
Pedro Rivaldo <pedrorivaldev@gmail.com>
2025-02-10 23:02:02 -0300

    interfaces e api requests para aluno

--- front/src/app/interfaces/aluno.ts
+++ front/src/app/interfaces/aluno.ts
@@ -1,43 +1,54 @@
-import { Turma } from "./models"
+import { GetTurmaResponse } from "./turma"
 
 export interface CreateAlunoRequest {
   nome: string
   email: string
   telefone: string
-  idTurma: Turma
+  idTurma: number
+}
+
+export interface CreateAlunoResponse {
+  id: number
+  nome: string
+  email: string
+  telefone: string
+  turma: GetTurmaResponse
+  situacao: string
+  ativo: boolean
 }
 
 export interface UpdateAlunoRequest {
   nome: string
   email: string
   telefone: string
-  idTurma: Turma
-  situacao: string
-}
-
-export interface CreateAlunoResponse {
-  id: number
-  idTurma: Turma
-  nome: string
-  email: string
-  telefone: string
-  situacao: string
-}
-
-export interface GetAlunoResponse {
-  id: number
-  idTurma: Turma
-  nome: string
-  email: string
-  telefone: string
+  idTurma: number
   situacao: string
 }
 
 export interface UpdateAlunoResponse {
   id: number
-  idTurma: Turma
   nome: string
   email: string
   telefone: string
+  turma: GetTurmaResponse
   situacao: string
+  ativo: boolean
+}
+
+export interface GetAlunoResponse {
+  id: number
+  nome: string
+  email: string
+  telefone: string
+  turma: GetTurmaResponse
+  situacao: string
+  ativo: boolean
+}
+
+export interface AlunoFiltros {
+  serie: number
+  turma: number
+  nome: string
+  situacao: string
+  ativo: boolean
 }
