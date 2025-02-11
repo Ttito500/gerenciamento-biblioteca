8e519ff99a75109192938dfb3c0e292abf17ce2f
Pedro Rivaldo <pedrorivaldev@gmail.com>
2025-02-10 23:01:54 -0300

    interfaces e api requests para turma

---
+++ front/src/app/interfaces/turma.ts
@@ -0,0 +1,43 @@
+export interface CreateTurmaRequest {
+  serie: number;
+  turma: string;
+  anoDeEntrada: number
+}
+
+export interface CreateTurmaResponse {
+  id: number
+  serie: number
+  turma: string
+  anoDeEntrada: number
+  ativo: boolean
+}
+
+export interface UpdateTurmaRequest {
+  serie: number
+  turma: string
+  anoDeEntrada: number
+  ativo: boolean
+}
+
+export interface UpdateTurmaResponse {
+  id: number
+  serie: number
+  turma: string
+  anoDeEntrada: number
+  ativo: boolean
+}
+
+export interface GetTurmaResponse {
+  id: number
+  serie: number
+  turma: string
+  anoDeEntrada: number
+  ativo: boolean
+}
+
+export interface TurmaFiltros {
+  serie: number
+  turma: string
+  anoDeEntrada: number
+  ativo: boolean
+}
