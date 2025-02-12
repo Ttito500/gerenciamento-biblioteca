b0bae6a0669826f6ab398043f134d0e0c15572c2
Tiago Tito <tiago.tito.sampaio@gmail.com>
2025-02-11 22:12:57 -0300

    dto ocorrencia e correcao nome variavel dto FrequenciaAlunos

--- bibliotech/bibliotech/src/main/java/com/bibliotech/bibliotech/dtos/response/OcorrenciaResponseDTO.java
+++ bibliotech/bibliotech/src/main/java/com/bibliotech/bibliotech/dtos/response/OcorrenciaResponseDTO.java
@@ -3,11 +3,14 @@ package com.bibliotech.bibliotech.dtos.response;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
-public class OcorrenciaResponceDTO {
+public class OcorrenciaResponseDTO {
     private Integer id;
     private AlunoResponseDTO aluno;
     private UsuarioResponseDTO registradaPor;
-
+    private String detalhes;
+    private LocalDate data;
 }
