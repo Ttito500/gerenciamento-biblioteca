c4966ffc10c797462dc149b575a0b26b03ba0635
Pedro Rivaldo <pedrorivaldev@gmail.com>
2025-02-19 23:35:41 -0300

    listagem de exemplares

--- front/src/app/pages/Acervo/Exemplares/templates/ListagemExemplares.tsx
+++ front/src/app/pages/Acervo/Exemplares/templates/ListagemExemplares.tsx
@@ -1,164 +1,79 @@
-import React, { useState } from "react";
-import { Table, Badge, Button, ButtonGroup } from "react-bootstrap";
+import React from "react";
+import { Table, Badge, Button, ButtonGroup, Tooltip, OverlayTrigger } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faEdit } from "@fortawesome/free-solid-svg-icons";
-import Modal from "react-bootstrap/esm/Modal";
-import EditarExemplar from "./EditarExemplar";
-import ConfirmarStatus from "./ConfirmarStatus";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { GetExemplarResponse } from "./../../../../interfaces/exemplar";
 
-const ListagemExemplares: React.FC = () => {
+interface ListagemExemplaresProps {
+  exemplares: GetExemplarResponse[];
+	onEdit: (exemplar: GetExemplarResponse) => void;
+}
 
-    const [showEditar, setShowEditar] = useState(false);
-    const handleCloseEditar = () => setShowEditar(false);
-    const handleShowEditar = () => setShowEditar(true);
+const ListagemExemplares: React.FC<ListagemExemplaresProps> = ({ exemplares, onEdit }) => {
 
+	const renderTooltipEditar = (props: any) => (
+    <Tooltip id="button-tooltip-5" {...props}>
+      Editar
+    </Tooltip>
+  );
 
-    const [ShowConfirmar, setShowConfirmar] = useState(false);
-    const handleCloseConfirma = () => setShowConfirmar(false);
-    const handleShowConfirmar = () => setShowConfirmar(true);
+  return (
+    <>
+      <Table striped className="tabela">
+        <thead>
+          <tr>
+            <th className="th-center-size-eight">Exemplar</th>
+            <th className="th-center-size-eight">Estante</th>
+            <th className="th-center-size-eight">Prateleira</th>
+            <th>Seção</th>
+            <th>Observações</th>
+            <th className="th-center-size-fifteen">Situação</th>
+            <th className="th-center-size-eight">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+					{exemplares?.map((exemplar) => (
+            <tr key={exemplar.id} className="tabela-tr">
+              <td className="text-center">{exemplar.numero}</td>
+              <td className="text-center">{exemplar.estanteprateleira.estante}</td>
+              <td className="text-center">{exemplar.estanteprateleira.prateleira}</td>
+              <td className="text-center">{exemplar.secao.nome}</td>
+              <td>{exemplar.observacao}</td>
+              <td className="text-center">
+                { exemplar.situacao === 'disponivel' && <Badge className="bibliotech-badge" bg={'success'}>
+                  Disponível
+                </Badge>}
 
-    return(
-      <>
-          <p className="fw-bold">Exemplares do Livro:</p>
+								{ exemplar.situacao === 'emprestado' && <Badge className="bibliotech-badge" bg={'info'}>
+                  Emprestado
+                </Badge>}
 
-          <Table striped className="tabela">
-              <thead>
-              <tr>
-                  <th className="th-center-size-eight">Exemplar</th>
-                  <th className="th-center-size-eight">Estante</th>
-                  <th className="th-center-size-eight">Prateleira</th>
-                  <th>Seção</th>
-                  <th>Observações</th>
-                  <th className="th-center-size-fifteen">Situação</th>
-                  <th className="th-center-size-eight">Ações</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr className="tabela-tr">
-                  <td className="text-center">1</td>
-                  <td className="text-center">A</td>
-                  <td className="text-center">2</td>
-                  <td>Romance</td>
-                  <td>Bom estado</td>
-                  <td className="text-center">
-                      <Badge className="bibliotech-badge" bg="green">
-                          Disponível
-                      </Badge>
-                  </td>
-                  <td className="text-center">
-                      <ButtonGroup aria-label="Ações" className="tabela-acoes">
-                          <Button
-                              onClick={handleShowEditar}
-                              variant="btn-outline-secondary"
-                              className="color-green"
-                          >
-                              <FontAwesomeIcon icon={faEdit} />
-                          </Button>
-                      </ButtonGroup>
-                  </td>
-              </tr>
-              <tr className="tabela-tr">
-                  <td className="text-center">2</td>
-                  <td className="text-center">A</td>
-                  <td className="text-center">2</td>
-                  <td>Romance</td>
-                  <td>Pequenos desgastes</td>
-                  <td className="text-center">
-                      <Badge className="bibliotech-badge" bg="blue">
-                          Emprestado
-                      </Badge>
-                  </td>
-                  <td className="text-center">
-                      <ButtonGroup aria-label="Ações" className="tabela-acoes">
-                          <Button
-                              onClick={handleShowEditar}
-                              variant="btn-outline-secondary"
-                              className="color-green"
-                          >
-                              <FontAwesomeIcon icon={faEdit} />
-                          </Button>
-                      </ButtonGroup>
-                  </td>
-              </tr>
-              <tr className="tabela-tr">
-                  <td className="text-center">3</td>
-                  <td className="text-center">A</td>
-                  <td className="text-center">2</td>
-                  <td>Romance</td>
-                  <td>Danificado</td>
-                  <td className="text-center">
-                      <Badge className="bibliotech-badge" bg="red">
-                          Extraviado
-                      </Badge>
-                  </td>
-                  <td className="text-center">
-                      <ButtonGroup aria-label="Ações" className="tabela-acoes">
-                          <Button
-                              onClick={handleShowEditar}
-                              variant="btn-outline-secondary"
-                              className="color-green"
-                          >
-                              <FontAwesomeIcon icon={faEdit} />
-                          </Button>
-                      </ButtonGroup>
-                  </td>
-              </tr>
-              </tbody>
-          </Table>
-
-          <Modal
-              show={showEditar}
-              onHide={handleCloseEditar}
-              size="lg"
-              backdrop="static"
-              centered
-              keyboard={false}
-          >
-              <Modal.Header closeButton>
-                  <Modal.Title>Editar Exemplar</Modal.Title>
-              </Modal.Header>
-
-              <Modal.Body>
-                  <EditarExemplar />
-              </Modal.Body>
-
-              <Modal.Footer>
-                  <Button variant="secondary" onClick={handleCloseEditar}>
-                      Cancelar
-                  </Button>
-                  <Button variant="success">
-                      <FontAwesomeIcon icon={faCheck} /> Salvar
-                  </Button>
-              </Modal.Footer>
-          </Modal>
-
-          <Modal
-              show={ShowConfirmar}
-              onHide={handleCloseConfirma}
-              size="lg"
-              backdrop="static"
-              centered
-              keyboard={false}
-              className="Modais-Confirmacao-Custon"
-          >
-              <Modal.Header closeButton>
-                  <Modal.Title>Confirmação</Modal.Title>
-              </Modal.Header>
-
-              <Modal.Body>
-                  <ConfirmarStatus />
-              </Modal.Body>
-
-              <Modal.Footer>
-                  <Button variant="secondary" onClick={handleCloseConfirma}>
-                      Cancelar
-                  </Button>
-                  <Button variant="danger">
-                      <FontAwesomeIcon icon={faCheck} /> Sim
-                  </Button>
-              </Modal.Footer>
-          </Modal>
-      </>
+								{ exemplar.situacao === 'extraviado' && <Badge className="bibliotech-badge" bg={'danger'}>
+                  Extraviado
+                </Badge>}
+              </td>
+              <td>
+                <ButtonGroup aria-label="Ações" className="tabela-acoes">
+                  <OverlayTrigger
+                    placement="right"
+                    delay={{ show: 250, hide: 400 }}
+                    overlay={renderTooltipEditar}
+                  >
+                    <Button
+                      variant="btn-outline-secondary"
+                      className="color-green"
+                      onClick={() => onEdit(exemplar)}
+                    >
+                      <FontAwesomeIcon icon={faPenToSquare} />
+                    </Button>
+                  </OverlayTrigger>
+                </ButtonGroup>
+              </td>
+            </tr>
+					))}
+        </tbody>
+      </Table>
+    </>
   );
 };
 
