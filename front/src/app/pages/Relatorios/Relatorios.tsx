161bdadf599853c9208dcd96be02bfb8a96119e6
kauan <kauancavalcante8p@gmail.com>
2025-02-22 03:56:41 -0300

    criando os arquivos

---
+++ front/src/app/pages/Relatorios/Relatorios.tsx
@@ -0,0 +1,13 @@
+const Relatorios: React.FC = () => {
+    return(
+        <section className="Exemplar">
+            <div className="w-100">
+                <h2>Teste</h2>
+
+            </div>
+
+        </section>
+    );
+};
+
+export default Relatorios;
