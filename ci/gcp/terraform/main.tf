0256938027f75688aade161124ef0c0022785740
Pedro Rivaldo <pedrorivaldev@gmail.com>
2025-02-23 12:18:25 -0300

    fix secrets

--- ci/gcp/terraform/main.tf
+++ ci/gcp/terraform/main.tf
@@ -8,13 +8,13 @@ module "instance" {
   db_user          = var.db_user
   db_port          = var.db_port 
   db_schema_name   = var.db_schema_name 
-  db_password      = data.google_secret_manager_secret_version.db_password_latest.secret_data
+  db_password      = var.db_password
 
   jdbc_url         = var.jdbc_url
   api_port         = var.api_port
 
   email_smtp       = var.email_smtp 
-  senha_email_smtp = data.google_secret_manager_secret_version.senha_email_smtp_latest.secret_data
+  senha_email_smtp = var.senha_email_smtp
 
-  jwt_secret       = data.google_secret_manager_secret_version.jwt_secret_latest.secret_data
+  jwt_secret       = var.jwt_secret
 }
