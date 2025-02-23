4ffaf4f47a89ae1e072673de9be119f28d5902c3
Pedro Rivaldo <pedrorivaldev@gmail.com>
2025-02-23 12:46:48 -0300

    metadata startup

--- ci/gcp/terraform/modules/instance/main.tf
+++ ci/gcp/terraform/modules/instance/main.tf
@@ -14,48 +14,23 @@ resource "google_compute_instance" "docker_instance" {
     access_config {}  # IP público
   }
 
-   metadata = {
-    repo_url    = var.repo_url
+  metadata = {
+    startup-script = templatefile("${path.module}/startup.sh.tpl", {
+      repo_url    = var.repo_url
 
-    db_name          = var.db_name
-    db_user          = var.db_user
-    db_port          = var.db_port 
-    db_schema_name   = var.db_schema_name 
-    db_password      = var.db_password
+      db_name          = var.db_name
+      db_user          = var.db_user
+      db_port          = var.db_port 
+      db_schema_name   = var.db_schema_name 
+      db_password      = var.db_password
 
-    jdbc_url         = var.jdbc_url
-    api_port         = var.api_port
+      jdbc_url         = var.jdbc_url
+      api_port         = var.api_port
 
-    email_smtp       = var.email_smtp 
-    senha_email_smtp = var.senha_email_smtp
+      email_smtp       = var.email_smtp 
+      senha_email_smtp = var.senha_email_smtp
 
-    jwt_secret       = var.jwt_secret
+      jwt_secret       = var.jwt_secret
+    })
   }
-
-  metadata_startup_script = <<-EOF
-  set -e
-
-  apt-get update && apt-get install -y docker.io docker-compose git
-
-  systemctl start docker
-
-  export DB_NAME="${db_name}"
-  export DB_USER="${db_user}"
-  export DB_PORT="${db_port}"
-  export JDBC_URL="${jdbc_url}"
-  export DB_SCHEMA_NAME="${db_schema_name}"
-  export API_PORT="${api_port}"
-  export EMAIL_SMTP="${email_smtp}"
-  export DB_PASSWORD="${db_password}"
-  export SENHA_EMAIL_SMTP="${senha_email_smtp}"
-  export JWT_SECRET="${jwt_secret}"
-
-  git clone "${repo_url}" /opt/app
-  cd /opt/app
-
-  cd bibliotech/bibliotech
-
-  # Roda apenas o banco e a API
-  docker-compose up -d --build postgres api
-  EOF
 }
